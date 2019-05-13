a234ce5f4a4720e725177818a469828d410a6106
chalidade <Chalidade@gmail.com>
2019-05-13 08:16:56 +0700

    Second commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Belajar Vue</title>
+    <!-- Include File Vue.js -->
+    <script src="vue.js"></script>
+  </head>
+  <body>
+    <div id="app">
+      {{ message }}
+    </div>
+  </body>
+  <!-- Include Main.js -->
+  <script src="main.js"></script>
+</html>
